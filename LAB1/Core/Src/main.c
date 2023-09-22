510bd7993c66c18cbee0e7990bb0a922531814ad
anhtr-nguyn <anhnguyen.0912a@gmail.com>
2023-09-22 17:39:38 +0700

    ex3 update

--- LAB1/Core/Src/main.c
+++ LAB1/Core/Src/main.c
@@ -47,12 +47,19 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+enum trafficLightState{
+	RED1_GREEN2,
+	RED1_YELLOW2,
+	GREEN1_RED2,
+	YELLOW1_RED2
+};
 
 /* USER CODE END 0 */
 
@@ -83,19 +90,72 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
 
+	enum trafficLightState currentState = RED1_GREEN2;
+	enum trafficLightState nextState = currentState;
+	int counter = 0;
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  if (counter <= 0){
+		  switch (currentState){
+		  case RED1_GREEN2:
+				  nextState = RED1_YELLOW2;
+				  counter = 2;
+	 			  HAL_GPIO_WritePin(RED1_GPIO_Port, RED1_Pin, RESET);
+	 			  HAL_GPIO_WritePin(YELLOW1_GPIO_Port, YELLOW1_Pin, SET);
+	 			  HAL_GPIO_WritePin(GREEN1_GPIO_Port, GREEN1_Pin, SET);
+	 			  HAL_GPIO_WritePin(RED2_GPIO_Port, RED2_Pin, SET);
+	 			  HAL_GPIO_WritePin(YELLOW2_GPIO_Port, YELLOW2_Pin, RESET);
+	 			  HAL_GPIO_WritePin(GREEN2_GPIO_Port, GREEN2_Pin, SET);
+				  break;
+			  case RED1_YELLOW2:
+				  nextState = GREEN1_RED2;
+				  counter = 3;
+	 			  HAL_GPIO_WritePin(RED1_GPIO_Port, RED1_Pin, SET);
+	 			  HAL_GPIO_WritePin(YELLOW1_GPIO_Port, YELLOW1_Pin, SET);
+	 			  HAL_GPIO_WritePin(GREEN1_GPIO_Port, GREEN1_Pin, RESET);
+	 			  HAL_GPIO_WritePin(RED2_GPIO_Port, RED2_Pin, RESET);
+	 			  HAL_GPIO_WritePin(YELLOW2_GPIO_Port, YELLOW2_Pin, SET);
+	 			  HAL_GPIO_WritePin(GREEN2_GPIO_Port, GREEN2_Pin, SET);
+				  break;
+			  case GREEN1_RED2:
+				  nextState = YELLOW1_RED2;
+				  counter = 2;
+	 			  HAL_GPIO_WritePin(RED1_GPIO_Port, RED1_Pin, SET);
+	 			  HAL_GPIO_WritePin(YELLOW1_GPIO_Port, YELLOW1_Pin, RESET);
+	 			  HAL_GPIO_WritePin(GREEN1_GPIO_Port, GREEN1_Pin, SET);
+	 			  HAL_GPIO_WritePin(RED2_GPIO_Port, RED2_Pin, RESET);
+	 			  HAL_GPIO_WritePin(YELLOW2_GPIO_Port, YELLOW2_Pin, SET);
+	 			  HAL_GPIO_WritePin(GREEN2_GPIO_Port, GREEN2_Pin, SET);
+	 			  break;
+			  case YELLOW1_RED2:
+				  nextState = RED1_GREEN2;
+				  counter = 3;
+	 			  HAL_GPIO_WritePin(RED1_GPIO_Port, RED1_Pin, RESET);
+	 			  HAL_GPIO_WritePin(YELLOW1_GPIO_Port, YELLOW1_Pin, SET);
+	 			  HAL_GPIO_WritePin(GREEN1_GPIO_Port, GREEN1_Pin, SET);
+	 			  HAL_GPIO_WritePin(RED2_GPIO_Port, RED2_Pin, SET);
+	 			  HAL_GPIO_WritePin(YELLOW2_GPIO_Port, YELLOW2_Pin, SET);
+	 			  HAL_GPIO_WritePin(GREEN2_GPIO_Port, GREEN2_Pin, RESET);
+	 			  break;
+			  default:
+				  break;
+		  }
+	  }
+	  currentState = nextState;
+	  counter = counter - 1;
+	  HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-  }
   /* USER CODE END 3 */
+}
 }
 
 /**
@@ -131,6 +191,33 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, RED1_Pin|YELLOW1_Pin|GREEN1_Pin|RED2_Pin
+                          |YELLOW2_Pin|GREEN2_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pins : RED1_Pin YELLOW1_Pin GREEN1_Pin RED2_Pin
+                           YELLOW2_Pin GREEN2_Pin */
+  GPIO_InitStruct.Pin = RED1_Pin|YELLOW1_Pin|GREEN1_Pin|RED2_Pin
+                          |YELLOW2_Pin|GREEN2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
