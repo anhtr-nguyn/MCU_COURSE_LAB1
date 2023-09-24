5c0baab56fcb806a04333dfb92a3f56a2320d0ee
anhtr-nguyn <anhnguyen.0912a@gmail.com>
2023-09-24 19:53:29 +0700

    update ex4 main.c comment

--- LAB1/Core/Src/main.c
+++ LAB1/Core/Src/main.c
@@ -54,6 +54,7 @@ static void MX_GPIO_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+// init enum type for state machine
 enum trafficLightState{
 	RED1_GREEN2,
 	RED1_YELLOW2,
